8733a3a00e70a726ad57877131bcfece7e68f3dc
baijanathTharu <bnchaudhary.tharu@gmail.com>
2022-06-13 14:05:12 +0545

    initialize admin dashboard app

---
+++ apps/admin/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import styles from '../index.module.css';
+
+export function Index() {
+  return (
+    <div className={styles.page}>
+      <h2 className="text-2xl font-bold my-4 text-center">
+        Admin Dashboard Login
+      </h2>
+    </div>
+  );
+}
+
+export default Index;
