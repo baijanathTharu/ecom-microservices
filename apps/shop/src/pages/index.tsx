54237351b467c50f78aec91d146bf02b8d2407ed
baijanathTharu <bnchaudhary.tharu@gmail.com>
2022-06-13 14:30:03 +0545

    initialize shop app

---
+++ apps/shop/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import styles from '../index.module.css';
+
+export function Index() {
+  return (
+    <div className={styles.page}>
+      <h2 className="text-2xl font-bold my-4 text-center">Shop page</h2>
+    </div>
+  );
+}
+
+export default Index;
