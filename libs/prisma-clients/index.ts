d4f433ff64766bfc277f96b3314550fe3d1bf769
baijanathTharu <bnchaudhary.tharu@gmail.com>
2022-06-13 16:46:54 +0545

    add a prisma generator and generate a prisma client for shop

---
+++ libs/prisma-clients/index.ts
@@ -0,0 +1,1 @@
+export { ShopModelClient } from './shop-model';
---
+++ libs/prisma-clients/shop-model/index.ts
@@ -0,0 +1,2 @@
+export { PrismaClient as ShopModelClient } from '.prisma/shop-model-client';
+export * from '.prisma/shop-model-client';
